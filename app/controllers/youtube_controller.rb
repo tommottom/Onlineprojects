82b16c5e1df2cd0621b1f7a1c8d8d288a05c369d
rudiments <fb53ice4j@gmail.com>
2015-07-25 13:41:53 +0000

    this is comit commet

---
+++ app/controllers/youtube_controller.rb
@@ -0,0 +1,13 @@
+class YoutubeController < ApplicationController
+    
+    def index
+       
+    end
+    
+    def search
+        query = params[:query]
+        youtube = Youtube.new
+        @results = youtube.retrieve_search_result(query)
+    end
+    
+end
---
+++ app/models/youtube.rb
@@ -0,0 +1,23 @@
+class Youtube < ActiveRecord::Base
+    
+    def retrieve_search_result(query)
+        mechanize = Mechanize.new { |agent|
+            agent.user_agent_alias = 'Mac Safari'
+        }
+
+        search_page = mechanize.get('https://www.youtube.com')
+        search_result = search_page.form_with(:id => 'masthead-search') do |search_form|
+            search_form['search_query'] = query
+        end.submit
+
+        results = Array.new
+        search_result.search('div.yt-lockup.yt-lockup-tile.yt-lockup-video.vve-check.yt-uix-tile').each do |div|
+            result = Hash.new
+            result['text'] = div.css('a.yt-uix-sessionlink.yt-uix-tile-link.yt-ui-ellipsis.yt-ui-ellipsis-2.spf-link').text
+            result['link'] = div.css('a.yt-uix-sessionlink.yt-uix-tile-link.yt-ui-ellipsis.yt-ui-ellipsis-2.spf-link').attribute('href').value
+            result['image'] = div.css('div.yt-thumb.video-thumb img').attribute('src').value
+            results << result
+        end
+        return results
+    end
+end
